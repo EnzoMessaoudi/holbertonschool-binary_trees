ed1a4d7d8b588dac910a82cbd1da98aea833cc85
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-09 16:37:44 +0100

    ADD 9 file

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - Check the height of a tree
+* @tree: Tree checked for his height
+* Return: Return the height of the tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t index_left;
+	size_t index_right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL  && tree->right == NULL)
+		return (0);
+
+	index_left = binary_tree_height(tree->left);
+	index_right = binary_tree_height(tree->right);
+
+	if (index_left > index_right)
+		return (1 + index_left);
+	else
+		return (1 + index_right);
+
+}
