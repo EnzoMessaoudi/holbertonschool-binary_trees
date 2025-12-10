8224e918206125e8b675e0eb7e1b122e3015973a
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-10 10:54:26 +0100

    FIX 15 file

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,11 +19,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	else if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	index_left = binary_tree_is_full(tree->right);
-	index_right = binary_tree_is_full(tree->left);
+	index_left = binary_tree_is_full(tree->left);
+	index_right = binary_tree_is_full(tree->right);
 
 	if (index_left != index_right)
 		return (0);
 
-	return (index_left);
+	return (1);
 }
